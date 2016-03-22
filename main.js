883eecb50850a4b8124fd8198a1f69c2b65a96ba
Doelia <kyxalia@gmail.com>
2016-03-22 14:26:13 +0100

    Show of projects : done

--- main.js
+++ main.js
@@ -20,23 +20,26 @@ class MultiGitStats {
               [ 'hash'
               , 'subject'
               , 'authorDate'
+              , 'committerDate'
               ]
             }
 
         gitlog(options, function (obj) {
             return function(error, commits) {
-                obj.onLogRecv(commits);
+                obj.onLogRecv(commits, path);
             };
         }(this));
     }
 
-    onLogRecv(commits) {
+    onLogRecv(commits, path) {
         this.cptRecv++;
         for (var i in commits) {
             var c = commits[i];
-            this.commits[i] = c;
+            c.repo = path;
+            this.commits.push(c);
         }
         if (this.cptRecv == this.cptLimit) {
+            console.log('ready');
             this.onReady();
         }
     }
@@ -59,10 +62,56 @@ class MultiGitStats {
 
 }
 
+class LogAnalyser {
+
+    constructor(listCommits) {
+        this.commits = listCommits;
+    }
+
+    show() {
+        for (var i in this.commits) {
+            var c = this.commits[i];
+            //console.log(c);
+            var d = new Date(c.authorDate);
+            console.log(d);
+        }
+    }
+
+    showProjects(commits) {
+        for (var i in commits) {
+            var c = commits[i];
+            console.log(c.authorDate+" - "+c.repo);
+        }
+    }
+
+    getCommitsOnDate(day, month, year) {
+        var commits = [];
+        var min = new Date(year, month, day);
+        console.log('Searched date : '+min);
+        var max = new Date(min);
+        max.setDate(max.getDate() + 1);
+
+        for (var i in this.commits) {
+            var c = this.commits[i];
+            var d = new Date(c.authorDate);
+            if (d < max && d > min) {
+                commits.push(c);
+            }
+        }
+
+        return commits;
+    }
+
+
+}
+
 var multiGitStats = new MultiGitStats();
 multiGitStats.recordGitRepo('/Users/doelia/Documents/dev/learning/dactylo');
 multiGitStats.recordGitRepo('/Users/doelia/Documents/dev/learning/multigit-stats');
 multiGitStats.buildLogs(function() {
-    console.log(this.commits);
+    var logAnalyser = new LogAnalyser(this.commits);
+    //logAnalyser.show();
+    var commits = logAnalyser.getCommitsOnDate(22, 2, 2016);
+    logAnalyser.showProjects(commits);
 });
 setTimeout(function() {}, 2000);
